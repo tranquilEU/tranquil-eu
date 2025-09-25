7bc30ec8a23a4accbf98e96ac7dcfacc126fec03
TranquilEU <tranquil.eu@protonmail.com>
2025-09-25 23:23:27 +0200

    🐞 fix: fix loading module error

--- frontend/src/shared/i18n/i18n.ts
+++ frontend/src/shared/i18n/i18n.ts
@@ -1,33 +1,33 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
-import { defaultNS, resources } from "./resources";
+import { defaultNS, resources } from '@/shared/i18n/resources';
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
 
 i18n
-  // i18next-http-backend
-  // loads translations from your server
-  // https://github.com/i18next/i18next-http-backend
-  .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
-  .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
-  .init({
-    debug: false,
-    lng: "en",
-    fallbackLng: "hr",
-    interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
-    },
-    backend: {
-      loadPath: "/locales/{{lng}}/{{ns}}.json", // where translations live
-    },
-    defaultNS,
-    resources,
-  });
+	// i18next-http-backend
+	// loads translations from your server
+	// https://github.com/i18next/i18next-http-backend
+	.use(Backend)
+	// detect user language
+	// learn more: https://github.com/i18next/i18next-browser-languageDetector
+	.use(LanguageDetector)
+	// pass the i18n instance to react-i18next.
+	.use(initReactI18next)
+	// init i18next
+	// for all options read: https://www.i18next.com/overview/configuration-options
+	.init({
+		debug: false,
+		lng: 'en',
+		fallbackLng: 'hr',
+		interpolation: {
+			escapeValue: false // not needed for react as it escapes by default
+		},
+		backend: {
+			loadPath: '/locales/{{lng}}/{{ns}}.json' // where translations live
+		},
+		defaultNS,
+		resources
+	});
 
 export default i18n;
